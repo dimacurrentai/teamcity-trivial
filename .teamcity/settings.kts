8d3599387b44d95490b0dcb23824098a91140442
Dima Korolev <dima@current.ai>
2026-01-08 23:02:33 -0800

    Three steps, not two.

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -2,32 +2,9 @@ import jetbrains.buildServer.configs.kotlin.*
 import jetbrains.buildServer.configs.kotlin.buildFeatures.perfmon
 import jetbrains.buildServer.configs.kotlin.buildSteps.script
 
-/*
-The settings script is an entry point for defining a TeamCity
-project hierarchy. The script should contain a single call to the
-project() function with a Project instance or an init function as
-an argument.
-
-VcsRoots, BuildTypes, Templates, and subprojects can be
-registered inside the project using the vcsRoot(), buildType(),
-template(), and subProject() methods respectively.
-
-To debug settings scripts in command-line, run the
-
-    mvnDebug org.jetbrains.teamcity:teamcity-configs-maven-plugin:generate
-
-command and attach your debugger to the port 8000.
-
-To debug in IntelliJ Idea, open the 'Maven Projects' tool window (View
--> Tool Windows -> Maven Projects), find the generate task node
-(Plugins -> teamcity-configs -> teamcity-configs:generate), the
-'Debug' option is available in the context menu for the task.
-*/
-
 version = "2025.11"
 
 project {
-
     buildType(Build)
 }
 
@@ -48,6 +25,11 @@ object Build : BuildType({
             name = "hw4"
             id = "hw4"
             scriptContent = "echo hw4"
+        }
+        script {
+            name = "hw5"
+            id = "hw5"
+            scriptContent = "echo hw5"
         }
     }
 
