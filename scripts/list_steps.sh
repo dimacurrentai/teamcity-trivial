1baf3be66de8e1c60d535ebb8d85cb012ca75a5a
Dima Korolev <dima@current.ai>
2026-01-10 22:18:02 -0800

    wip

--- scripts/list_steps.sh
+++ scripts/list_steps.sh
@@ -2,4 +2,4 @@
 set -euo pipefail
 
 ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
-(cd "$ROOT_DIR"; ls steps/*.sh | sort) > "${ROOT_DIR}/.steps_list.txt.tmp"
+(cd "$ROOT_DIR"; ls steps/*.sh | sort)
